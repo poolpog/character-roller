dbc7c23f1c2d4fcd53b86ff947c4322ea2be5294
Pool Pog <poolpog@gmail.com>
2017-12-28 13:16:07 -0500

    added bonuses and print layout:

--- character-roller.py
+++ character-roller.py
@@ -24,6 +24,14 @@ class Character():
 
 class Attribute():
     value = 0
+    bonus_strings = {
+        'strength':     '{:>+2d} melee,{:>+2d} dmg,{:>+3.0f}% xp',
+        'intelligence': '{:>+2d} languages,{:>+3.0f}% xp',
+        'wisdom':       '{:>+2d} save v spell,{:>+3.0f}% xp',
+        'dexterity':    '{:>+2d} missle,{:>+2d} AC,{:>+3.0f}% xp',
+        'constitution': '{:>+2d} HP',
+        'charisma':     '{:>+2d} NPC react'
+    }
 
     def __init__(self):
         self.roll()
@@ -71,15 +79,113 @@ class Attribute():
             out = +.10
         return out
 
+class HitPoints():
+    value = 1
+    d_dict = {
+        'fighter': 8,
+        'thief': 4,
+        'magicuser': 4,
+        'cleric': 6,
+        'elf': 6,
+        'dwarf': 8,
+        'halfling': 6,
+        'mystic': 6,
+    }
+
+
 def main():
     new_character = Character()
 
     attribute_names = [ "strength", "intelligence", "wisdom", "dexterity", "constitution", "charisma" ]
 
-    for name in attribute_names:
-        print "{:>13} => {:2}".format( name.title(), new_character.attributes[name].value)
-        print "{:>30} => {:>+3d}".format( (name + " bonus").title(), new_character.attributes[name].skill_bonus())
-        print "{:>30} => {:>+3.0f}%".format( (name + " XP bonus").title(), ( new_character.attributes[name].xp_bonus() *100 ))
+    #for name in attribute_names:
+        #print "{:>13} => {:2}".format( name.title(), new_character.attributes[name].value)
+        #print "{:>30} => {:>+3d}".format( (name + " bonus").title(), new_character.attributes[name].skill_bonus())
+        #print "{:>30} => {:>+3.0f}%".format( (name + " XP bonus").title(), ( new_character.attributes[name].xp_bonus() *100 ))
+
+    strength_bonus = ""
+    intelligence_bonus = ""
+    wisdom_bonus = ""
+    dexterity_bonus = ""
+    constitution_bonus = ""
+    charisma_bonus = ""
+    if new_character.attributes['strength'].skill_bonus() != 0:
+        strength_bonus = new_character.attributes['strength'].bonus_strings['strength'].format(
+            new_character.attributes['strength'].skill_bonus(),
+            new_character.attributes['strength'].skill_bonus(),
+            new_character.attributes['strength'].xp_bonus()*100
+            )
+
+    if new_character.attributes['intelligence'].skill_bonus() != 0:
+        intelligence_bonus = new_character.attributes['intelligence'].bonus_strings['intelligence'].format(
+            new_character.attributes['intelligence'].skill_bonus(),
+            new_character.attributes['intelligence'].xp_bonus()*100
+            )
+
+    if new_character.attributes['wisdom'].skill_bonus() != 0:
+        wisdom_bonus = new_character.attributes['wisdom'].bonus_strings['wisdom'].format(
+            new_character.attributes['wisdom'].skill_bonus(),
+            new_character.attributes['wisdom'].xp_bonus()*100
+            )
+
+    if new_character.attributes['dexterity'].skill_bonus() != 0:
+        dexterity_bonus = new_character.attributes['dexterity'].bonus_strings['dexterity'].format(
+            new_character.attributes['dexterity'].skill_bonus(),
+            new_character.attributes['dexterity'].skill_bonus(),
+            new_character.attributes['dexterity'].xp_bonus()*100
+            )
+
+    if new_character.attributes['constitution'].skill_bonus() != 0:
+        constitution_bonus = new_character.attributes['constitution'].bonus_strings['constitution'].format(
+            new_character.attributes['constitution'].skill_bonus()
+            )
+
+    if new_character.attributes['charisma'].skill_bonus() != 0:
+        charisma_bonus = new_character.attributes['charisma'].bonus_strings['charisma'].format(
+            new_character.attributes['charisma'].skill_bonus()
+            )
+
+    print "+----------------------------------------------+-------------------+".format()
+    print "| Name:__________________________|Money        |Sketch             |".format()
+    print "| Player:________________________|PP:___GP:___ |                   |".format()
+    print "| Class:_________________________|EP:___SP:___ |                   |".format()
+    print "| Alignment:_____________________|CP:___       |                   |".format()
+    print "| LVL:__ HP:__ AC:__ XP:__ Sex:__|OTHER:_____  |                   |".format()
+    print "|                                              |                   |".format()
+    print "|                                     Saving   |                   |".format()
+    print "| Attribs   Modifiers                 Throws   |                   |".format()
+    print "|     +--+  +-----------------------+ Poison+--+                   |".format()
+    print "| STR:|{:>2}|  |{:<23}| Death |  |                   |".format(new_character.attributes['strength'].value,strength_bonus)
+    print "|     +--+  +-----------------------+ Ray   +--+                   |".format()
+    print "|     +--+  +-----------------------+          |                   |".format()
+    print "| INT:|{:>2}|  |{:23}| Magic +--+                   |".format(new_character.attributes['intelligence'].value,intelligence_bonus)
+    print "|     +--+  +-----------------------+ Wand  |  |-- Weapons/Armor --+".format()
+    print "|     +--+  +-----------------------+       +--+                   |".format()
+    print "| WIS:|{:>2}|  |{:23}|          |                   |".format(new_character.attributes['wisdom'].value,wisdom_bonus)
+    print "|     +--+  +-----------------------+ Stone/+--+                   |".format()
+    print "|     +--+  +-----------------------+ Paral-+  |                   |".format()
+    print "| DEX:|{:>2}|  |{:23}| ysis  +--+                   |".format(new_character.attributes['dexterity'].value,dexterity_bonus)
+    print "|     +--+  +-----------------------+          |                   |".format()
+    print "|     +--+  +-----------------------+ Dragon+--+                   |".format()
+    print "| CON:|{:>2}|  |{:23}| Breath|  |                   |".format(new_character.attributes['constitution'].value,constitution_bonus)
+    print "|     +--+  +-----------------------+       +--+-- Normal Items ---|".format()
+    print "|     +--+  +-----------------------+          |                   |".format()
+    print "| CHA:|{:>2}|  |{:23}| Spell +--+                   |".format(new_character.attributes['charisma'].value,charisma_bonus)
+    print "|     +--+  +-----------------------+ Magic |  |                   |".format()
+    print "| Languages:_________________________ Staff +--+                   |".format()
+    print "| Cleric or Thief Skills:                      |                   |".format()
+    print "|                                              |                   |".format()
+    print "+-To Hit AC------------------------------------+                   |".format()
+    print "|  9 | 8 | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 | -1  |                   |".format()
+    print "+----------------------------------------------+-- Magic Items ----|".format()
+    print "|    |   |   |   |   |   |   |   |   |   |     |                   |".format()
+    print "+----+---+---+---+---+---+---+---+---+---+-----+                   |".format()
+    print "| Notes                                        |                   |".format()
+    print "|                                              |                   |".format()
+    print "|                                              |                   |".format()
+    print "|                                              |                   |".format()
+    print "+----------------------------------------------+-------------------+".format()
+
 
 
 ##################################
